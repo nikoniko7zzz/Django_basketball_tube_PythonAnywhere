f1310d7b8220c344b374eb5947a89366e5c30cb9
matsuo <matsuokuniko7@gmail.com>
2022-11-19 22:34:18 +0900

    item form label fix

--- base/forms/item_forms.py
+++ base/forms/item_forms.py
@@ -9,10 +9,10 @@ class ItemCreateForm(forms.ModelForm):
         model = Item
         # 表示順番
         fields  = ('youtube_url', 'tag', 'shooting_date', 'title', 'description')
-        labels = {'youtube_url':'Youtube ID', 'tag':'タグ', 'shooting_date':'撮影日', 'title':'動画タイトル', 'description':'動画説明'}
+        labels = {'youtube_url':'Youtube ID(https://youtu.be/ここの部分)', 'tag':'タグ', 'shooting_date':'撮影日', 'title':'動画タイトル', 'description':'動画説明'}
         widgets = {
             'youtube_url': forms.TextInput(
-                attrs={'class': 'form-control mb-3  col-md-4', 'placeholder': '英数字11文字 (https://youtu.be/ここの部分)',}),
+                attrs={'class': 'form-control mb-3  col-md-4', 'placeholder': '英数字11文字',}),
             'tag': forms.Select(
                 attrs={'class': 'form-select mb-3 col-md-4',}),
             'shooting_date': forms.DateTimeInput(
